90a94610d8e26a5d89ab99e7744c589506864253
Jerry Yang <hy0kle@gmail.com>
2015-07-08 17:02:14 +0800

    add qa 环境自动部署

--- pandora-deploy.sh
+++ pandora-deploy.sh
@@ -7,9 +7,14 @@
 user=work
 #hosts_conf='192.168.64.10 192.168.64.11'
 hosts_conf='192.168.64.10'
-path="/home/work/higo/higo"
-
+path="/home/work/higo/higo "
 . deploy-git.sh deploy
+
+qas="/home/work/higo/higo-wenyy /home/work/higo/higo-liuyang /home/work/higo/higo-chelh /home/work/higo/higo-lichong /home/work/higo/higo-houbl"
+for path in $qas
+do
+    . deploy-git.sh deploy
+done
 
 user=root
 hosts_conf='192.168.190.2'
