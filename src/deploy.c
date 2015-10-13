8bd1a7363a68b19cc56dec6c27fe0ea9fa98db0a
Jerry Yang <hy0kle@gmail.com>
2015-10-13 18:16:24 +0800

    完成配置文件的解析.

--- src/deploy.c
+++ src/deploy.c
@@ -45,6 +45,7 @@ typedef struct _g_cfg_t
     char user[BUF_LEN];
     char path[BUF_LEN];
 
+    int host_count;
     cJSON *hosts_conf;
 } g_cfg_t;
 
@@ -65,6 +66,7 @@ char *NORMAL   = "\e[0m";
 #endif
 
 #define printfln(format, arg...) fprintf(stderr, format"\n", ##arg)
+#define print_error(format, arg...) fprintf(stderr, "%s"format"%s\n", RED, ##arg, NORMAL)
 
 /** 全局变量 */
 g_cfg_t g_cfg;
@@ -109,7 +111,82 @@ parse_arg(int argc, char *argv[])
     snprintf(cfg_file, PATH_LEN, "conf/%s.json", g_cfg.project);
     logprintf("cfg_file: %s", cfg_file);
 
+    //从文件中读取要解析的JSON数据
+    FILE *fp = fopen(cfg_file, "r");
+    if (NULL == fp)
+    {
+        print_error("Can NOT open config file: %s", cfg_file);
+        exit(111);
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char *data = (char*)malloc(len + 1);
+    if (NULL == data)
+    {
+        print_error("Can NOT malloc for json paser.");
+
+        fclose(fp);
+        exit(112);
+    }
+
+    fread(data, 1, len, fp);
+    fclose(fp);
+
+    //解析JSON数据
+    cJSON *root_json = cJSON_Parse(data);    //将字符串解析成json结构体
+    if (NULL == root_json)
+    {
+        print_error("parse JSON error: %s", cJSON_GetErrorPtr());
+
+        cJSON_Delete(root_json);
+        free(data);
+        //exit(JSON_PARSE_FAILURE);
+        exit(113);
+    }
+
+    cJSON *user = cJSON_GetObjectItem(root_json, "user");
+    if (NULL == user)
+    {
+        print_error("Lost config: user");
+        goto PARSE_EXCEPTION;
+    }
+    snprintf(g_cfg.user, BUF_LEN, "%s", user->valuestring);
+    logprintf("g_cfg.user: %s", g_cfg.user);
+
+    cJSON *path = cJSON_GetObjectItem(root_json, "path");
+    if (NULL == path)
+    {
+        print_error("Lost config: path");
+        goto PARSE_EXCEPTION;
+    }
+    snprintf(g_cfg.path, BUF_LEN, "%s", path->valuestring);
+    logprintf("g_cfg.path: %s", g_cfg.path);
+
+    g_cfg.hosts_conf = cJSON_GetObjectItem(root_json, "hosts_conf");
+    if (NULL == g_cfg.hosts_conf)
+    {
+        print_error("Lost config: hosts_conf");
+        goto PARSE_EXCEPTION;
+    }
+    g_cfg.host_count = cJSON_GetArraySize(g_cfg.hosts_conf);
+    logprintf("g_cfg.host_count = %d", g_cfg.host_count);
+    if (! (g_cfg.host_count > 0 ))
+    {
+        print_error("Need config host.");
+        goto PARSE_EXCEPTION;
+    }
+
+    cJSON_Delete(root_json);
+    free(data);
+
     return 0;
+
+PARSE_EXCEPTION:
+    cJSON_Delete(root_json);
+    free(data);
+    return 114;
 }
 
 int main(int argc, char *argv[])
